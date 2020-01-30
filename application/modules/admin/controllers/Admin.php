6389c62448c4b59b7e318e196dae67f6853f9495
Rafi Khalish <rafidev30@gmail.com>
2020-01-30 10:49:13 +0700

    Admin

---
+++ application/modules/admin/controllers/Admin.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_admin');
+    }
+
+    public function index()
+    {
+        $useremail = $this->session->userdata('email');
+        $data['title'] = 'Home Admin';
+        $data['user'] = $this->m_admin->getUser('users', $useremail);
+        
+        //$this->db->get_where('users',['user_email' => $this->session->userdata('email')])->row_array();
+        
+        $this->load->view('templates/header',$data);
+        $this->load->view('templates/sidebar',$data);
+        $this->load->view('templates/topbar',$data);
+        $this->load->view('v_admin', $data);
+        $this->load->view('templates/footer');
+    }
+
+}
