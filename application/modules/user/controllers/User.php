f74388d6c0d496e3c0fd67a8e9bb8824bbfe2f8d
Rafi Khalish <rafidev30@gmail.com>
2020-01-30 10:49:00 +0700

    User

--- application/modules/user/controllers/User.php
+++ application/modules/user/controllers/User.php
@@ -2,11 +2,25 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_user');
+    }
 
     public function index()
     {
-        $data['user'] = $this->db->get_where('users',['user_email' => $this->session->userdata('email')])->row_array();
-        echo 'Cilupbaa' .$data['user']['user_id'];
+        $useremail = $this->session->userdata('email');
+        $data['title'] = 'User';
+        $data['user'] = $this->m_user->getUser('users', $useremail);
+        
+        //$this->db->get_where('users',['user_email' => $this->session->userdata('email')])->row_array();
+        
+        $this->load->view('templates/header',$data);
+        $this->load->view('templates/sidebar',$data);
+        $this->load->view('templates/topbar',$data);
+        $this->load->view('v_user', $data);
+        $this->load->view('templates/footer');
     }
 
 }
