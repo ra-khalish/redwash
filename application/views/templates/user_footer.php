5aa2fe293a95c3547f0b0e8e47f7402a2b22c1ea
Rafi Khalish <rafidev30@gmail.com>
2020-03-15 17:07:38 +0700

    Update home & User transaction

--- application/views/templates/user_footer.php
+++ application/views/templates/user_footer.php
@@ -107,9 +107,11 @@
           {"data": "status",
             "render": function (data, type, row, meta) {
               if(data === 'Queue'){
-                var label = 'badge-info';
+                var label = 'badge-primary';
               }else if (data === 'Processed'){
                 var label = 'badge-warning';
+              }else if (data === 'Completed'){
+                var label = 'badge-info';
               }else{
                 label = 'badge-success';
               }
