5a205093ad4735bf313ab16aba4e7bb204a8613b
Rafi Khalish <rafidev30@gmail.com>
2020-08-11 21:34:58 +0700

    Clean home code

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -2,22 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('m_home');
-    }
-
     public function index()
     {
-        $useremail = $this->session->userdata('email');
-        $data['title'] = 'Dashboard';
-        $data['user'] = $this->m_home->getUser('users', $useremail);
-        
-        //$this->db->get_where('users',['user_email' => $this->session->userdata('email')])->row_array();
-        
-        $this->load->view('templates/home_header',$data);
-        $this->load->view('v_home', $data);
+        $this->load->view('templates/home_header');
+        $this->load->view('v_home');
         $this->load->view('templates/home_footer');
     }
 
