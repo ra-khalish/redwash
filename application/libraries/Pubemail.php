c4daab239c8b53c221d35cebdbabb5916ab88af2
Rafi Khalish <rafidev30@gmail.com>
2021-01-12 05:52:23 +0700

    Update properties

--- application/libraries/Pubemail.php
+++ application/libraries/Pubemail.php
@@ -3,17 +3,30 @@
 require_once("./vendor/autoload.php");
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Wire\AMQPTable;
 
 class Pubemail
 {
     
     function processSend($data)
     {  
-        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $connection = new AMQPStreamConnection('172.20.0.2', 5672, 'admin', 'admin');
         $channel = $connection->channel();
 
-        $channel->queue_declare('task_queue', false, true, false, false);
-
+        $channel->exchange_declare(
+            'myexchange',
+            'direct',
+            true,
+            false
+        );
+        $channel->queue_declare(
+            'myqueue',
+            false, true, false, false, 
+            // new AMQPTable(array(
+            //     "x-queue-type" => "classic",
+            //     "x-message-ttl" => 600)
+            // )
+        );
         // $data = implode(' ', array_slice($argv, 1));
         if (empty($data)) {
             $data = "Hello World!";
@@ -23,7 +36,7 @@ class Pubemail
             array('content_type' => 'application/json','delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
         );
 
-        $channel->basic_publish($msg, '', 'task_queue');
+        $channel->basic_publish($msg, 'myexchange', 'mykey');
 
         echo $data;
 
