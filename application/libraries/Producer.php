009041ce0028a2f88b81f2843c29eb3859d4dbc3
Rafi Khalish <rafidev30@gmail.com>
2021-08-27 23:53:49 +0700

    changing rabbitmq credential

--- application/libraries/Producer.php
+++ application/libraries/Producer.php
@@ -1,89 +1,52 @@
 <?php
 
-// require_once("./vendor/autoload.php");
-// use PhpAmqpLib\Connection\AMQPStreamConnection;
-// use PhpAmqpLib\Message\AMQPMessage;
-// use PhpAmqpLib\Wire\AMQPTable;
-
-// class Pubemail
-// {
-    
-//     function processSend($data)
-//     {  
-//         $connection = new AMQPStreamConnection('127.0.0.1', 5672, 'guest', 'guest');
-//         $channel = $connection->channel();
-
-//         $channel->exchange_declare(
-//             'myexchange',
-//             'direct',
-//             true,
-//             false
-//         );
-//         $channel->queue_declare(
-//             'myqueue',
-//             false, true, false, false, 
-//             // new AMQPTable(array(
-//             //     "x-queue-type" => "classic",
-//             //     "x-message-ttl" => 600)
-//             // )
-//         );
-//         $data = implode(' ', array_slice($argv, 1));
-//         if (empty($data)) {
-//             $data = "Hello World!";
-//         }
-//         $msg = new AMQPMessage(
-//             $data,
-//             array('content_type' => 'application/json','delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
-//         );
-
-//         $channel->basic_publish($msg, 'myexchange', 'mykey');
-
-//         echo $data;
-
-//         $channel->close();
-//         $connection->close();
-
-//         return TRUE;
-//     }    
-// }
-
 require_once("./vendor/autoload.php");
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Exchange\AMQPExchangeType;
 use PhpAmqpLib\Message\AMQPMessage;
 
 class Producer
-{
-    
-    function publish($data)
+{       
+    function publish($message)
     {  
-        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        
+        $connection = new AMQPStreamConnection(
+            'localhost', 5682, 'admin', 'admin', 'mail');
         $channel = $connection->channel();
 
-        # Mutate data
-        $data = json_decode($data, TRUE);
 
+        // # Initialize Broker
+        // $exchange_name = 'notif_event';
+        // if ($message['message_type'] == 'notif') {
+        //     $queue_name = 'notif.status.queue';
+        //     $routing_key = 'notif.status';
+        // } elseif ($message['message_type'] == 'auth'){
+        //     $queue_name = 'notif.auth.queue';
+        //     $routing_key = 'notif.auth';
+        // }
 
         # Initialize Broker
-        $exchange_name = 'myexchange';
-        $queue_name = 'myqueue';
-        $key = 'mykey';
+        $exchange_name = 'notif_event';
+        $queue_name = 'notif.email.queue';
+        $routing_key = 'notif.email';
 
         $channel->queue_declare($queue_name, false, true, false, false);
         $channel->exchange_declare($exchange_name, AMQPExchangeType::DIRECT, false, true, false);
-        $channel->queue_bind($queue_name, $exchange_name, $key);
+        $channel->queue_bind($queue_name, $exchange_name, $routing_key);
         // $data = implode(' ', array_slice($argv, 1));
-        if (empty($data)) {
-            $data = "Hello World!";
+
+        $message = json_encode($message);
+        if (empty($message)) {
+            $message = "Hello World!";
         }
         $msg = new AMQPMessage(
-            $data,
+            $message,
             array('content_type' => 'application/json','delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
         );
 
-        $channel->basic_publish($msg, $exchange_name, $key);
-
-        echo $data;
+        $channel->basic_publish($msg, $exchange_name, $routing_key);
+        echo $message;
+        log_message('info', $message);
 
         $channel->close();
         $connection->close();
@@ -91,4 +54,3 @@ class Producer
         return TRUE;
     }    
 }
-
